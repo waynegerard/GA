d206fb005930ec485b36dcb197e52d50b46f2de9
Lilly Chen <lillychen@ip-192-168-1-188.us-east-2.compute.internal>
2020-05-27 13:45:44 -0400

    Deletes all skills except for music, 8ball, roll, and fortnite drop

--- Client/src/skills/index.ts
+++ Client/src/skills/index.ts
@@ -1,22 +1,12 @@
 import MusicPlayer from './fun/music'
-import DotaCounters from './dota/counters'
-import Odds from './fun/odds'
-import SoundBoard from './fun/sounds'
 import RandomNoInput from './fun/randomNoInput'
 import FortniteDropLocation from './fortnite/dropLocation'
-import ApexDropLocation from './apex/apexDrop'
-import AreYouGay from './fun/areyougay'
-import Hotseat from './fun/hotseat'
 import BaseSkill from './baseSkill'
-import Credit from './meta/credit'
-import Learn from './learn/learn'
 import JoinVoice from './meta/join'
 import LeaveVoice from './meta/leave'
-import Slap from './easterEggs/slap'
-import OverwatchHeroPicker from './overwatch/heroPicker'
 
-const skillList: any[] = [DotaCounters, FortniteDropLocation, ApexDropLocation, OverwatchHeroPicker, MusicPlayer,
-  RandomNoInput, Odds, AreYouGay, Hotseat, Learn, SoundBoard, Credit, JoinVoice, LeaveVoice, Slap]
+const skillList: any[] = [FortniteDropLocation, OverwatchHeroPicker, MusicPlayer,
+  RandomNoInput, JoinVoice, LeaveVoice]
 
 const skillInstances = () => skillList.map((skill: any): BaseSkill => new (<any>skill)())
 
